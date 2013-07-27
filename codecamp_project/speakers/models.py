766f94b9ae10ffe22eceac719914bf98c5714431
Tyler Harms <tyler.harms@gmail.com>
2013-07-27 18:25:11 -0500

    Adds the Speakers app

--- codecamp_project/speakers/models.py
+++ codecamp_project/speakers/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Speaker(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['last_name']
+
+    def _get_full_name(self):
+        """Returns the speaker's full name."""
+        return '%s %s' % (self.first_name, self.last_name)
+
+    full_name = property(_get_full_name)
+
+    def __unicode__(self):
+        return self.full_name
+
+    def get_absolute_url(self):
+        return ('speaker_detail', (), {'slug': self.slug,
+                                       'id': self.id})
+    get_absolute_url = models.permalink(get_absolute_url)
