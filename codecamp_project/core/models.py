dff2bda150071d981365b242618146437369d7de
Tyler Harms <tyler.harms@gmail.com>
2013-07-27 18:28:52 -0500

    Adds a 'core' use app

---
+++ codecamp_project/core/models.py
@@ -0,0 +1,14 @@
+# core/models.py
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that proviedes self-
+    updating 'created' and 'modified' fields
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
