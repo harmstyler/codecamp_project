8f367737d67b739fb1b11d9d214fbd910ccc5dfa
Tyler Harms <tyler.harms@gmail.com>
2013-08-04 15:46:36 -0500

    Updates speaker test to verify all speaker content is being sent in the response

--- codecamp_project/speakers/tests.py
+++ codecamp_project/speakers/tests.py
@@ -2,6 +2,14 @@ from django.test import TestCase
 
 
 class SpeakersViewsTestCase(TestCase):
+    fixtures = ['speakers_views_testdata.json']
+
     def test_index(self):
         resp = self.client.get('/speakers/')
         self.assertEqual(resp.status_code, 200)
+        self.assertTrue('speakers' in resp.context)
+        self.assertEqual([speaker.pk for speaker in resp.context['speakers']], [1])
+        speaker_1 = resp.context['speakers'][0]
+        self.assertEqual(speaker_1.first_name, 'Tyler')
+        self.assertEqual(speaker_1.last_name, 'Harms')
+        self.assertEqual(speaker_1.slug, 'tyler-harms')
