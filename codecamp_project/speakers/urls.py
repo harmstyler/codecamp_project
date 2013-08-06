d29c203fd5ef4e301b1253282a824ac6e68505f8
Tyler Harms <tyler.harms@gmail.com>
2013-08-05 21:28:29 -0500

    makes the speaker view tests pass

--- codecamp_project/speakers/urls.py
+++ codecamp_project/speakers/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<id>\d+)/$', views.detailID, name='speaker_detail_id'),
+    url(r'^(?P<slug>[-\w\d]+),(?P<id>\d+)/$', views.detail, name='speaker_detail'),
 )
--- codecamp_project/speakers/views.py
+++ codecamp_project/speakers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from django import template
 
@@ -13,3 +13,12 @@ def index(request):
         raise Http404
     return render(request, 'speakers/index.html',
         {'speakers': speakers})
+
+def detailID(request, id):
+    speaker = get_object_or_404(Speaker, pk=id)
+    return redirect(speaker)
+
+def detail(request, slug, id):
+    speaker = get_object_or_404(Speaker, pk=id)
+    return render(request, 'speakers/detail.html',
+        {'speaker': speaker})
